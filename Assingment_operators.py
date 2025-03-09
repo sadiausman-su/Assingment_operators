568d3d7c1332bbd849bc46bdc7ef57329daf61e0
sadiausman-su <usmanbodla047@gmail.com>
2025-03-09 04:25:15 +0000

    Added Assingment_operators

---
+++ Assingment_operators.py
@@ -0,0 +1,10 @@
+#assingment operators
+#  =   >>>use to assing the value to variable  
+# +=    >>>use to increment in the value 
+# -=   >>>>use to decrement in the value
+x=5
+print(x)
+x+=3
+print(x)
+x-=3
+print(x)
